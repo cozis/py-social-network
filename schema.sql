c2718a2a2170fa3e09198a265b486c4466de3694
Francesco Cozzuto <heibyob@gmail.com>
2022-05-04 21:25:49 +0200

    more schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,35 @@
-CREATE TABLE Category(
-    name        VARCHAR(32) PRIMARY KEY,
+CREATE TABLE Category (
+           name VARCHAR(32) PRIMARY KEY,
     description VARCHAR(512)
 );
 
-CREATE TABLE User(
-    username VARCHAR(32) PRIMARY KEY,
-    password VARCHAR(128) NOT NULL
-    email    VARCHAR(512),
-    website  TEXT
+CREATE TABLE User (
+     username VARCHAR(32) PRIMARY KEY,
+     password VARCHAR(128) NOT NULL
+    pvt_email VARCHAR(512),
+    pub_email VARCHAR(512),
+     website TEXT
 );
 
-CREATE TABLE Post(
-    
+CREATE TABLE Post (
+          id INTEGER PRIMARY KEY,
+    username VARCHAR(32), NOT NULL,
+    category VARCHAR(32), 
+       title VARCHAR(128) NOT NULL,
+        body TEXT,
+        link TEXT,
+       time_ TIMESTAMP NOT NULL,
+    FOREIGN KEY (username) REFERENCES User(username),
+    FOREIGN KEY (category) REFERENCES Category(name),
+    CHECK (body XOR link)
+);
+
+CREATE TABLE Reply (
+        id   INTEGER PRIMARY KEY,
+      post   INTEGER NOT NULL,
+    parent   INTEGER,
+      body      TEXT NOT NULL,
+     time_ TIMESTAMP NOT NULL,
+    FOREIGN KEY (post) REFERENCES Post(id),
+    FOREIGN KEY (parent) REFERENCES Reply(id)
 );
